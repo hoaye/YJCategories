e19d456050fddb9f696595b32113dbdbdc880c85
yuejun.hou <8069060@qq.com>
2017-02-19 14:12:47 +0800

    kdkd

---
+++ YJCategoriesGather/YJCategoriesGather/YJCategories/Foundation/NSNotificationCenter/NSNotificationCenter+YJMainThread.m
@@ -0,0 +1,37 @@
+//
+//  NSNotificationCenter+YJMainThread.m
+//  YJCategoriesGather
+//
+//  Created by YJHou on 2017/2/19.
+//  Copyright © 2017年 侯跃军 houmanager@Hotmail.com. All rights reserved.
+//
+
+#import "NSNotificationCenter+YJMainThread.h"
+
+@implementation NSNotificationCenter (YJMainThread)
+
+/** 在主线程中发送一条通知 */
+- (void)postNotificationOnMainThread:(NSNotification *)notification{
+    
+    [self performSelectorOnMainThread:@selector(postNotification:) withObject:notification waitUntilDone:YES];
+}
+
+/** 在主线程中发送一条通知 携带对象 */
+- (void)postNotificationOnMainThreadWithName:(NSString *)name
+                                      object:(id)anObject{
+    
+    NSNotification *notification = [NSNotification notificationWithName:name object:anObject];
+    [self postNotificationOnMainThread:notification];
+}
+
+/** 在主线程中发送一条通知 携带对象和用户信息 */
+- (void)postNotificationOnMainThreadWithName:(NSString *)name
+                                     object:(id)anObject
+                                   userInfo:(NSDictionary *)userInfo{
+    
+    NSNotification *notification = [NSNotification notificationWithName:name object:anObject userInfo:userInfo];
+    [self postNotificationOnMainThread:notification];
+}
+
+
+@end
