adf26e9b62bd1422fc09e9bde221e43fc75fa2c0
yuejun.hou <8069060@qq.com>
2017-03-03 11:02:59 +0800

    26

--- YJCategoriesGather/YJCategoriesGather/ViewController.m
+++ YJCategoriesGather/YJCategoriesGather/ViewController.m
@@ -22,37 +22,14 @@
 
 - (void)setUpMainView{
 
-    NSString *urlString = @"http://wwww.baidu.com?a=1&b=2&c=3&d=4";
-    NSDictionary *dict = @{@"1":@"a",
-                           @"2":@"b",
-                           @"3":@"c",
-                           @"4":@"d"
-                           };
-    NSArray * keys1 = @[@"2", @"3", @"4", @"5"];
-    NSArray * keys2 = @[@"3", @"4"];
-    
-    NSURL *url = [NSURL URLWithString:urlString];
-    
-    NSMutableArray *mutableArray = [NSMutableArray array];
-    
-    [mutableArray yj_addSafeObject:@2];
-    
-    [mutableArray yj_addBOOL:NO];
-    
-    [mutableArray yj_addChar:'G'];
-    
-    [mutableArray yj_addInt:-112];
-    
-    [mutableArray yj_addRect:CGRectMake(10, 12, 13, 14)];
-    [mutableArray yj_addFloat:12.0f];
-    [mutableArray yj_addCGFloat:16.0f];
-    
-    [mutableArray yj_addInteger:-10];
 
+    NSString *appPath = [[NSBundle mainBundle] appIconPath];
     
-    NSLog(@"-->%@", mutableArray);
+    NSLog(@"-->%@", appPath);
     
-    NSLog(@"-->%c", [[mutableArray yj_objectSafeAtIndex:2] charValue]);
+    UIImage *appImg = [[NSBundle mainBundle] appIconImage];
+    
+    NSLog(@"-->%@", appImg);
     
 
 
