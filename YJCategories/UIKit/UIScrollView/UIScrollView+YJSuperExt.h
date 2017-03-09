01aea8070680873cf65c8876ed326f4ee2d77bff
yuejun.hou <8069060@qq.com>
2017-03-09 16:54:18 +0800

    54514

--- YJCategories/UIKit/UIScrollView/UIScrollView+YJSuperExt.h
+++ YJCategories/UIKit/UIScrollView/UIScrollView+YJSuperExt.h
@@ -10,4 +10,31 @@
 
 @interface UIScrollView (YJSuperExt)
 
+/** 共多少页 */
+- (NSInteger)yj_allPages;
+
+/** 当前是第几页 */
+- (NSInteger)yj_currentPage;
+
+/** 滚动百分比 */
+- (CGFloat)yj_scrollPercent;
+
+/** x方向上的页数 */
+- (CGFloat)yj_pagesX;
+
+/** y方向上的页数 */
+- (CGFloat)yj_pagesY;
+
+/** X当前是第几页 */
+- (CGFloat)yj_currentPageX;
+
+/** Y当前是第几页 */
+- (CGFloat)yj_currentPageY;
+
+/** 设置X方向页数 */
+- (void)yj_setPageX:(NSInteger)page animated:(BOOL)animated;
+
+/** 设置Y方向页数 */
+- (void)yj_setPageY:(NSInteger)page animated:(BOOL)animated;
+
 @end
--- YJCategories/UIKit/UIScrollView/UIScrollView+YJSuperExt.m
+++ YJCategories/UIKit/UIScrollView/UIScrollView+YJSuperExt.m
@@ -10,4 +10,62 @@
 
 @implementation UIScrollView (YJSuperExt)
 
+- (NSInteger)yj_allPages{
+    NSInteger pages = self.contentSize.width/self.frame.size.width;
+    return pages;
+}
+
+- (NSInteger)yj_currentPage{
+    NSInteger pages = self.contentSize.width/self.frame.size.width;
+    CGFloat scrollPercent = [self yj_scrollPercent];
+    NSInteger currentPage = (NSInteger)roundf((pages-1)*scrollPercent);
+    return currentPage;
+}
+
+- (CGFloat)yj_scrollPercent{
+    CGFloat width = self.contentSize.width-self.frame.size.width;
+    CGFloat scrollPercent = self.contentOffset.x/width;
+    return scrollPercent;
+}
+
+- (CGFloat)yj_pagesY{
+    CGFloat pageHeight = self.frame.size.height;
+    CGFloat contentHeight = self.contentSize.height;
+    return contentHeight/pageHeight;
+}
+
+- (CGFloat)yj_pagesX{
+    CGFloat pageWidth = self.frame.size.width;
+    CGFloat contentWidth = self.contentSize.width;
+    return contentWidth/pageWidth;
+}
+
+- (CGFloat)yj_currentPageY{
+    CGFloat pageHeight = self.frame.size.height;
+    CGFloat offsetY = self.contentOffset.y;
+    return offsetY / pageHeight;
+}
+
+- (CGFloat)yj_currentPageX{
+    CGFloat pageWidth = self.frame.size.width;
+    CGFloat offsetX = self.contentOffset.x;
+    return offsetX / pageWidth;
+}
+
+- (void)yj_setPageY:(NSInteger)page animated:(BOOL)animated{
+    CGFloat pageHeight = self.frame.size.height;
+    CGFloat offsetY = page * pageHeight;
+    CGFloat offsetX = self.contentOffset.x;
+    CGPoint offset = CGPointMake(offsetX,offsetY);
+    [self setContentOffset:offset];
+}
+
+- (void)yj_setPageX:(NSInteger)page animated:(BOOL)animated{
+    CGFloat pageWidth = self.frame.size.width;
+    CGFloat offsetY = self.contentOffset.y;
+    CGFloat offsetX = page * pageWidth;
+    CGPoint offset = CGPointMake(offsetX,offsetY);
+    [self setContentOffset:offset animated:animated];
+}
+
 @end
