bb44fa6510848145a3301b00abf50d0870c0c34f
yuejun.hou <8069060@qq.com>
2017-05-22 15:25:18 +0800

    时间格式

--- YJCategories/Foundation/NSDateFormatter/NSDateFormatter+YJSuperExt.h
+++ YJCategories/Foundation/NSDateFormatter/NSDateFormatter+YJSuperExt.h
@@ -16,11 +16,16 @@
 /** 根据 date 和 format 返回字符串日期 */
 + (NSString *)yj_stringWithDate:(NSDate *)date format:(NSString *)format;
 
+#pragma mark - 日期格式部分
 /** yyyy-MM-dd 格式 */
 + (NSString *)yj_yyyyMMddFormat;
 /** HH:mm:ss 格式 */
 + (NSString *)yj_HHmmssFormat;
 /** 完整的 yyyy-MM-dd HH:mm:ss */
 + (NSString *)yj_yyyyMMddHHmmssFormat;
+/** 月日年 格式 */
++ (NSString *)yj_MMddyyyyFormat;
+/** 带毫秒的完整的时间格式 */
++ (NSString *)yj_yyyyMMddHHmmssSSSFormat;
 
 @end
--- YJCategories/Foundation/NSDateFormatter/NSDateFormatter+YJSuperExt.m
+++ YJCategories/Foundation/NSDateFormatter/NSDateFormatter+YJSuperExt.m
@@ -22,19 +22,46 @@
     return [self _yj_stringWithDate:date format:format];
 }
 
+/** 
+ G:公元时代，例如AD公元
+ yy:年的后2位
+ yyyy:完整年
+ MM:月，显示为1-12
+ MMM:月，显示为英文月份简写,如Jan
+ MMMM:月，显示为英文月份全称，如Janualy
+ dd:日，2位数表示，如02
+ d:日，1-2位显示，如2
+ EEE:简写星期几，如Sun
+ EEEE:全写星期几，如Sunday
+ aa:上下午，AM/PM
+ H:时，24小时制，0-23
+ K：时，12小时制，0-11
+ m:分，1-2位
+ mm:分，2位
+ s:秒，1-2位
+ ss:秒，2位
+ S:毫秒
+ */
+
++ (NSString *)yj_MMddyyyyFormat{
+    return @"MMddyyyy";
+}
+
 /** yyyy-MM-dd 格式 */
 + (NSString *)yj_yyyyMMddFormat{
     return @"yyyy-MM-dd";
 }
 
-/** HH:mm:ss 格式 */
 + (NSString *)yj_HHmmssFormat{
     return @"HH:mm:ss";
 }
 
-/** 完整的 yyyy-MM-dd HH:mm:ss */
 + (NSString *)yj_yyyyMMddHHmmssFormat{
     return @"yyyy-MM-dd HH:mm:ss";
+}
+
++ (NSString *)yj_yyyyMMddHHmmssSSSFormat{
+    return @"yyyy-MM-ddHH:mm:ss.SSS";
 }
 
 #pragma mark - Private
