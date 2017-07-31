8196c97f7160a13da41d6fd971945969b62b6fc7
yuejun.hou <houmanager@qq.com>
2017-07-31 17:25:23 +0800

    commit at Company

--- YJCategories/UIKit/UIButton/UIButton+YJImagePosition.m
+++ YJCategories/UIKit/UIButton/UIButton+YJImagePosition.m
@@ -18,16 +18,14 @@
     
     CGFloat imageWidth = self.imageView.image.size.width;
     CGFloat imageHeight = self.imageView.image.size.height;
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    CGFloat labelWidth = [self.titleLabel.text sizeWithFont:self.titleLabel.font].width;
-    CGFloat labelHeight = [self.titleLabel.text sizeWithFont:self.titleLabel.font].height;
-#pragma clang diagnostic pop
     
-    CGFloat imageOffsetX = (imageWidth + labelWidth) / 2 - imageWidth / 2;//image中心移动的x距离
-    CGFloat imageOffsetY = imageHeight / 2 + spacing / 2;//image中心移动的y距离
-    CGFloat labelOffsetX = (imageWidth + labelWidth / 2) - (imageWidth + labelWidth) / 2;//label中心移动的x距离
-    CGFloat labelOffsetY = labelHeight / 2 + spacing / 2;//label中心移动的y距离
+    CGFloat labelWidth = [self.titleLabel.text sizeWithAttributes:@{NSFontAttributeName:self.titleLabel.font}].width;
+    CGFloat labelHeight = [self.titleLabel.text sizeWithAttributes:@{NSFontAttributeName:self.titleLabel.font}].height;
+    
+    CGFloat imageOffsetX = (imageWidth + labelWidth) * 0.5 - imageWidth * 0.5;//image中心移动的x距离
+    CGFloat imageOffsetY = imageHeight * 0.5 + spacing * 0.5;//image中心移动的y距离
+    CGFloat labelOffsetX = (imageWidth + labelWidth * 0.5) - (imageWidth + labelWidth) * 0.5;//label中心移动的x距离
+    CGFloat labelOffsetY = labelHeight * 0.5 + spacing * 0.5;//label中心移动的y距离
     
     CGFloat tempWidth = MAX(labelWidth, imageWidth);
     CGFloat changedWidth = labelWidth + imageWidth - tempWidth;
@@ -36,27 +34,27 @@
     
     switch (postion) {
         case YJButtomImagePositionLeft:
-            self.imageEdgeInsets = UIEdgeInsetsMake(0, -spacing/2, 0, spacing/2);
-            self.titleEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, -spacing/2);
-            self.contentEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, spacing/2);
+            self.imageEdgeInsets = UIEdgeInsetsMake(0, -spacing*0.5, 0, spacing*0.5);
+            self.titleEdgeInsets = UIEdgeInsetsMake(0, spacing*0.5, 0, -spacing*0.5);
+            self.contentEdgeInsets = UIEdgeInsetsMake(0, spacing*0.5, 0, spacing*0.5);
             break;
             
         case YJButtomImagePositionRight:
-            self.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth + spacing/2, 0, -(labelWidth + spacing/2));
-            self.titleEdgeInsets = UIEdgeInsetsMake(0, -(imageWidth + spacing/2), 0, imageWidth + spacing/2);
-            self.contentEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, spacing/2);
+            self.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth + spacing*0.5, 0, -(labelWidth + spacing*0.5));
+            self.titleEdgeInsets = UIEdgeInsetsMake(0, -(imageWidth + spacing*0.5), 0, imageWidth + spacing*0.5);
+            self.contentEdgeInsets = UIEdgeInsetsMake(0, spacing*0.5, 0, spacing*0.5);
             break;
             
         case YJButtomImagePositionTop:
             self.imageEdgeInsets = UIEdgeInsetsMake(-imageOffsetY, imageOffsetX, imageOffsetY, -imageOffsetX);
             self.titleEdgeInsets = UIEdgeInsetsMake(labelOffsetY, -labelOffsetX, -labelOffsetY, labelOffsetX);
-            self.contentEdgeInsets = UIEdgeInsetsMake(imageOffsetY, -changedWidth/2, changedHeight-imageOffsetY, -changedWidth/2);
+            self.contentEdgeInsets = UIEdgeInsetsMake(imageOffsetY, -changedWidth*0.5, changedHeight-imageOffsetY, -changedWidth*0.5);
             break;
             
         case YJButtomImagePositionBottom:
             self.imageEdgeInsets = UIEdgeInsetsMake(imageOffsetY, imageOffsetX, -imageOffsetY, -imageOffsetX);
             self.titleEdgeInsets = UIEdgeInsetsMake(-labelOffsetY, -labelOffsetX, labelOffsetY, labelOffsetX);
-            self.contentEdgeInsets = UIEdgeInsetsMake(changedHeight-imageOffsetY, -changedWidth/2, imageOffsetY, -changedWidth/2);
+            self.contentEdgeInsets = UIEdgeInsetsMake(changedHeight-imageOffsetY, -changedWidth*0.5, imageOffsetY, -changedWidth*0.5);
             break;
             
         default:
