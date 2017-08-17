b7cb453c6f5c729aebcfb401cd19ef097bc5d1a1
yuejun.hou <houmanager@qq.com>
2017-08-17 16:21:57 +0800

    commit at CW

--- YJCategories/Foundation/NSString/NSString+Hashing.h
+++ YJCategories/Foundation/NSString/NSString+Hashing.h
@@ -10,15 +10,4 @@
 
 @interface NSString (Hashing)
 
-@property (readonly) NSString *md5String;
-@property (readonly) NSString *sha1String;
-@property (readonly) NSString *sha256String;
-@property (readonly) NSString *sha512String;
-
-- (NSString *)hmacSHA1StringWithKey:(NSString *)key;
-- (NSString *)hmacSHA256StringWithKey:(NSString *)key;
-- (NSString *)hmacSHA512StringWithKey:(NSString *)key;
-
-- (NSString *)MD5Hash;
-
 @end
